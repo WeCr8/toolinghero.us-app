f37f151a84b2c3d73735394dd2c6fa99019c2a0d
WeCr8 <zach@wecr8.info>
2025-05-22 06:41:59 -0700

    Update AboutView.vue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<!-- src/views/AboutView.vue -->
+<template>
+  <section class="min-h-screen flex flex-col justify-center items-center
+    px-4 text-center">
+    <hi class="text-4x1 font-bold text-blue-600 mb-4">About ooling Hero</h1>
+      <p class="text-lg text-gray-700 dark:text-gray-300 max-w-2x1">
+        Tooling Hero empowers manufacurrere by simplifying the process of naming, tracking, and managing CNC tool assemlbies
+        through modern digital workflows, DANG helps standardize naming across systems like Mastercam, ZOLLER, and yout ERP.
+      </p>
+  </section>
+</template>
+
+<script setup lang="ts">
+</script>
+    
