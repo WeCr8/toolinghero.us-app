93bcb6f420cce01abb7befa4e1ed3ad9f4227bb5
WeCr8 <zach@wecr8.info>
2025-05-23 01:16:36 -0700

    update functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,27 @@
 /**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * Tooling Hero – Firebase Functions Index
+ * ---------------------------------------
+ * This is the entry point for all Cloud Functions.
+ * Each function is modularized into its own file within /src/
+ * For more info, see: https://firebase.google.com/docs/functions/typescript
  */
 
 import { onRequest } from 'firebase-functions/v2/https'
 import * as logger from 'firebase-functions/logger'
+
+// 🔔 Stripe webhook handler for billing events
 export { onStripeWebhook } from './onStripeWebhook'
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// 👤 Auth trigger – sets up user and default team on signup
+export { onUserCreate } from './onUserCreate'
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+// 🛠️ Tool creation – logs tool creation into DARN logs
+export { onToolAdd } from './onToolAdd'
+
+// 📌 NOTE: Utility functions like assignModulesByPlan are imported where used, not exported here
+
+// 🧪 Optional: Uncomment to test deployment or connectivity
+// export const helloWorld = onRequest((req, res) => {
+//   logger.info("Hello logs!", { structuredData: true })
+//   res.send("Hello from Firebase!")
 // });
